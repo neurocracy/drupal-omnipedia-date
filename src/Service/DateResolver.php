187a275e49dd0608d5fe3854185db7c7d4577ae5
Matei Stanca <i@ambientimpact.com>
2023-07-26 02:31:16 -0400

    Issue #1: Added DateResolver service.

---
+++ src/Service/DateResolver.php
@@ -0,0 +1,89 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\omnipedia_date\Service;
+
+use Drupal\Component\Datetime\DateTimePlus;
+use Drupal\omnipedia_date\Plugin\Omnipedia\Date\OmnipediaDateInterface;
+use Drupal\omnipedia_date\Service\CurrentDateInterface;
+use Drupal\omnipedia_date\Service\DateCollectionInterface;
+use Drupal\omnipedia_date\Service\DateResolverInterface;
+use Drupal\omnipedia_date\Service\DefaultDateInterface;
+use Drupal\omnipedia_date\Service\DefinedDatesInterface;
+
+/**
+ * The Omnipedia date resolver servivce.
+ */
+class DateResolver implements DateResolverInterface {
+
+  /**
+   * Service constructor; saves dependencies.
+   *
+   * @param \ Drupal\omnipedia_date\Service\CurrentDateInterface $currentDate
+   *   The Omnipedia current date service.
+   *
+   * @param \Drupal\omnipedia_date\Service\DateCollectionInterface $dateCollection
+   *   The Omnipedia date collection service.
+   *
+   * @param \Drupal\omnipedia_date\Service\DefaultDateInterface $defaultDate
+   *   The Omnipedia default date service.
+   *
+   * @param \Drupal\omnipedia_date\Service\DefinedDatesInterface $definedDates
+   *   The Omnipedia defined dates service.
+   */
+  public function __construct(
+    protected readonly CurrentDateInterface     $currentDate,
+    protected readonly DateCollectionInterface  $dateCollection,
+    protected readonly DefaultDateInterface     $defaultDate,
+    protected readonly DefinedDatesInterface    $definedDates,
+  ) {}
+
+  /**
+   * {@inheritdoc}
+   */
+  public function resolve(
+    string $dateOrKeyword, bool $includeUnpublished = false,
+  ): OmnipediaDateInterface {
+
+    if ($dateOrKeyword === 'current') {
+
+      $resolvedDate = $this->currentDate->get();
+
+    } else if ($dateOrKeyword === 'default') {
+
+      $resolvedDate = $this->defaultDate->get();
+
+    } else if ($dateOrKeyword === 'first') {
+
+      $resolvedDate = $this->definedDates->getFirstDate($includeUnpublished);
+
+    } else if ($dateOrKeyword === 'last') {
+
+      $resolvedDate = $this->definedDates->getLastDate($includeUnpublished);
+
+    } else {
+
+      /** @var \Drupal\Component\Datetime\DateTimePlus */
+      $dateObject = new DateTimePlus($dateOrKeyword, null);
+
+      if ($dateObject->hasErrors()) {
+
+        throw new \InvalidArgumentException(\sprintf(
+          'There were one or more errors attempting to parse "%s" into %s object:%s',
+          $dateOrKeyword,
+          DateTimePlus::class,
+          "\n" . \implode("\n", $dateObject->getErrors()),
+        ));
+
+      }
+
+      return $this->dateCollection->getFromDateTimeObject($dateObject);
+
+    }
+
+    return $this->dateCollection->get($resolvedDate);
+
+  }
+
+}
---
+++ src/Service/DateResolverInterface.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\omnipedia_date\Service;
+
+use Drupal\omnipedia_date\Plugin\Omnipedia\Date\OmnipediaDateInterface;
+
+/**
+ * The Omnipedia date resolver servivce interface.
+ */
+interface DateResolverInterface {
+
+  /**
+   * Resolve a date or keyword to an explicit date string.
+   *
+   * @param string $dateOrKeyword
+   *   Must be one of:
+   *
+   *   - 'current': Indicates the current date is to be used. This is the
+   *     default.
+   *
+   *   - 'default': Indicates the default date is to be used.
+   *
+   *   - 'first': Indicates that the first defined date is to be used.
+   *
+   *   - 'last': Indicates that the last defined date is to be used.
+   *
+   *   - A string that can be parsed by \Drupal\Component\Datetime\DateTimePlus
+   *     without errors.
+   *
+   * @param bool $includeUnpublished
+   *   Whether to include dates that have only unpublished content. This is used
+   *   if $date is 'first' or 'last'. Defaults to false.
+   *
+   * @return \Drupal\omnipedia_date\Plugin\Omnipedia\Date\OmnipediaDateInterface
+   *   The Omnipedia date plug-in for the resolved date.
+   *
+   * @throws \InvalidArgumentException
+   *   When $dateOrKeyword does not match a recognized keyword and cannot be
+   *   parsed into a valid date object.
+   */
+  public function resolve(
+    string $dateOrKeyword, bool $includeUnpublished = false,
+  ): OmnipediaDateInterface;
+
+}
