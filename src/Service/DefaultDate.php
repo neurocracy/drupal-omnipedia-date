2ca9513ec9f2893cb9ecae92d088913e71df820b
Matei Stanca <i@ambientimpact.com>
2023-08-10 01:20:50 -0400

    Default date no longer retrieved from configured front page.
    See https://github.com/neurocracy/omnipedia/issues/35

--- src/Service/DefaultDate.php
+++ src/Service/DefaultDate.php
@@ -5,8 +5,6 @@ declare(strict_types=1);
 namespace Drupal\omnipedia_date\Service;
 
 use Drupal\Core\State\StateInterface;
-use Drupal\omnipedia_core\Service\WikiNodeMainPageInterface;
-use Drupal\omnipedia_core\Service\WikiNodeResolverInterface;
 use Drupal\omnipedia_date\Service\DefaultDateInterface;
 use Drupal\omnipedia_date\Service\DateCollectionInterface;
 
@@ -33,31 +31,22 @@ class DefaultDate implements DefaultDateInterface {
    * @param \Drupal\omnipedia_date\Service\DateCollectionInterface $dateCollection
    *   The Omnipedia date collection service.
    *
-   * @param \Drupal\omnipedia_core\Service\WikiNodeMainPageInterface $wikiNodeMainPage
-   *   The Omnipedia wiki node main page service.
-   *
-   * @param \Drupal\omnipedia_core\Service\WikiNodeResolverInterface $wikiNodeResolver
-   *   The Omnipedia wiki node resolver service.
-   *
    * @param \Drupal\Core\State\StateInterface $stateManager
    *   The Drupal state system manager.
    */
   public function __construct(
-    protected readonly DateCollectionInterface    $dateCollection,
-    protected readonly WikiNodeMainPageInterface  $wikiNodeMainPage,
-    protected readonly WikiNodeResolverInterface  $wikiNodeResolver,
-    protected readonly StateInterface             $stateManager,
+    protected readonly DateCollectionInterface  $dateCollection,
+    protected readonly StateInterface           $stateManager,
   ) {}
 
   /**
-   * Find and set the default date if it hasn't yet been set.
+   * Find the default date if it hasn't yet been set.
    *
-   * @see self::setDefaultDate()
-   *   Validates and sets the default date.
+   * @see self::set()
+   *   Sets the default date.
    *
    * @throws \UnexpectedValueException
-   *   Exception thrown when a date cannot be retrieved from the front page
-   *   node.
+   *   If a default date has not been set.
    */
   protected function find(): void {
 
@@ -72,33 +61,16 @@ class DefaultDate implements DefaultDateInterface {
     // If we got a string instead of null, assume it's a date string, set it,
     // and return.
     if (\is_string($stateString) && !empty($stateString)) {
+
       $this->set($stateString);
 
       return;
+
     }
 
-    // If there's no default date set in the site state, we have to try to infer
-    // it from the default front page.
-
-    /** @var \Drupal\omnipedia_core\Entity\NodeInterface|null */
-    $defaultMainPage = $this->wikiNodeMainPage->getMainPage('default');
-
-    if (!$this->wikiNodeResolver->isWikiNode($defaultMainPage)) {
-      throw new \UnexpectedValueException(
-        'The default front page configured in the site settings does not appear to be a wiki page node.',
-      );
-    }
-
-    /** @var string|null */
-    $nodeDate = $defaultMainPage->getWikiNodeDate();
-
-    if ($nodeDate === null) {
-      throw new \UnexpectedValueException(
-        'Could not read the default date from the default main page node.',
-      );
-    }
-
-    $this->set($nodeDate);
+    throw new \UnexpectedValueException(
+      'No default date has been set!',
+    );
 
   }
 
