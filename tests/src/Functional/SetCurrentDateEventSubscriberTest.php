a7d501398b442f39d8ca761a8455b5c62419a77d
Matei Stanca <i@ambientimpact.com>
2023-09-14 14:33:53 -0400

    Issue #12: SetCurrentDateEventSubscriberTest up and running:
    This currently only tests canonical routes for nodes; other routes will
    be added soon.

--- tests/src/Functional/SetCurrentDateEventSubscriberTest.php
+++ tests/src/Functional/SetCurrentDateEventSubscriberTest.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Drupal\Tests\omnipedia_date\Functional;
 
+use Drupal\omnipedia_core\Entity\WikiNodeInfo;
 use Drupal\omnipedia_core\Service\WikiNodeTrackerInterface;
+use Drupal\omnipedia_date_current_date_test\EventSubscriber\Kernel\CurrentDateHeaderEventSubscriber;
 use Drupal\omnipedia_date\Service\CurrentDateInterface;
 use Drupal\omnipedia_date\Service\DefaultDateInterface;
 use Drupal\Tests\BrowserTestBase;
@@ -23,23 +25,34 @@ class SetCurrentDateEventSubscriberTest extends BrowserTestBase {
   use WikiNodeProvidersTrait;
 
   /**
+   * The default date to use in tests.
+   *
+   * The default date must be set for the current date service to work
+   * correctly, as it needs it as a fallback. We're using a date that's not
+   * expected to be used in the wiki nodes generated for this test.
+   *
+   * @see \Drupal\Tests\omnipedia_core\Traits\WikiNodeProvidersTrait::generateWikiDates()
+   */
+  protected const DEFAULT_DATE = '1989-12-16';
+
+  /**
    * The Drupal state key where we store the list of dates defined by content.
    */
   protected const DEFINED_DATES_STATE_KEY = 'omnipedia.defined_dates';
 
-  // /**
-  //  * The Omnipedia current date service.
-  //  *
-  //  * @var \Drupal\omnipedia_date\Service\CurrentDateInterface
-  //  */
-  // protected readonly CurrentDateInterface $currentDate;
+  /**
+   * The Omnipedia current date service.
+   *
+   * @var \Drupal\omnipedia_date\Service\CurrentDateInterface
+   */
+  protected readonly CurrentDateInterface $currentDate;
 
-  // /**
-  //  * The Omnipedia default date service.
-  //  *
-  //  * @var \Drupal\omnipedia_date\Service\DefaultDateInterface
-  //  */
-  // protected readonly DefaultDateInterface $defaultDate;
+  /**
+   * The Omnipedia default date service.
+   *
+   * @var \Drupal\omnipedia_date\Service\DefaultDateInterface
+   */
+  protected readonly DefaultDateInterface $defaultDate;
 
   /**
    * The Typed Entity repository manager.
@@ -63,7 +76,9 @@ class SetCurrentDateEventSubscriberTest extends BrowserTestBase {
   /**
    * {@inheritdoc}
    */
-  protected static $modules = ['omnipedia_date'];
+  protected static $modules = [
+    'omnipedia_date', 'omnipedia_date_current_date_test',
+  ];
 
   /**
    * {@inheritdoc}
@@ -72,18 +87,26 @@ class SetCurrentDateEventSubscriberTest extends BrowserTestBase {
 
     parent::setUp();
 
-    // // Set the defined dates to state so that the Omnipedia defined dates
-    // // service finds those and doesn't attempt to build them from the wiki node
-    // // tracker, which would return no values as we haven't created any wiki
-    // // nodes for this test.
-    // $this->container->get('state')->set(self::DEFINED_DATES_STATE_KEY, [
-    //   'all'       => static::$definedDatesData,
-    //   'published' => static::$definedDatesData,
-    // ]);
+    $definedDates = static::generateWikiDates();
 
-    // $this->currentDate = $this->container->get('omnipedia_date.current_date');
+    // Prepend the default date so that the date deriver includes it when
+    // generating derivatives.
+    //
+    // @see \Drupal\omnipedia_date\Plugin\Deriver\OmnipediaDateDeriver
+    \array_unshift($definedDates, self::DEFAULT_DATE);
 
-    // $this->defaultDate = $this->container->get('omnipedia_date.default_date');
+    // Set the defined dates to state so that the Omnipedia defined dates
+    // service finds those and doesn't attempt to build them from the wiki node
+    // tracker, which would return no values as we haven't created any wiki
+    // nodes yet.
+    $this->container->get('state')->set(self::DEFINED_DATES_STATE_KEY, [
+      'all'       => $definedDates,
+      'published' => $definedDates,
+    ]);
+
+    $this->currentDate = $this->container->get('omnipedia_date.current_date');
+
+    $this->defaultDate = $this->container->get('omnipedia_date.default_date');
 
     $this->typedEntityRepositoryManager = $this->container->get(
       'Drupal\typed_entity\RepositoryManager',
@@ -93,9 +116,8 @@ class SetCurrentDateEventSubscriberTest extends BrowserTestBase {
       'omnipedia.wiki_node_tracker'
     );
 
-    // $this->testUser = $this->drupalCreateUser([
-    //   'access content',
-    // ]);
+    // Set the default date, but use a date
+    $this->defaultDate->set(self::DEFAULT_DATE);
 
     $this->drupalCreateContentType(['type' => 'page']);
 
@@ -113,6 +135,87 @@ class SetCurrentDateEventSubscriberTest extends BrowserTestBase {
 
   }
 
-  public function testNavigateAnonymous(): void {}
+  /**
+   * Data provider for canonical route test, defining different user types.
+   *
+   * @return array
+   */
+  public static function canonicalRouteUserTypeProvider(): array {
+
+    return [
+      [false], // === anonymous, i.e. don't create a user account.
+      [[['access content']]],
+      [[[
+        'access content',
+        'edit any ' . WikiNodeInfo::TYPE . ' content',
+      ]]],
+    ];
+
+  }
+
+  /**
+   * Test that visiting various wiki node canonical routes updates current date.
+   *
+   * @dataProvider canonicalRouteUserTypeProvider
+   *
+   * @todo Also test that the current date remains that of the last wiki node
+   *   visited when visiting a non-wiki node.
+   */
+  public function testCanonicalRoute(bool|array $userParameters): void {
+
+    if (\is_array($userParameters)) {
+
+      $user = \call_user_func_array(
+        [$this, 'drupalCreateUser'], $userParameters,
+      );
+
+      $this->drupalLogin($user);
+
+    }
+
+    $parameters = static::generateWikiNodeValues();
+
+    /** @var \Drupal\node\NodeInterface[] Node objects keyed by their nid. */
+    $nodes = [];
+
+    foreach ($parameters as $values) {
+
+      /** @var \Drupal\node\NodeInterface */
+      $node = $this->drupalCreateNode($values);
+
+      $this->wikiNodeTracker->trackWikiNode($node);
+
+      $nodes[$node->id()] = $node;
+
+      // Roughly 1 out of 3 times, insert a non-wiki 'page' node type.
+      if (\rand(1, 3) === 1) {
+
+        /** @var \Drupal\node\NodeInterface */
+        $node = $this->drupalCreateNode(['type' => 'page']);
+
+        $nodes[$node->id()] = $node;
+
+      }
+
+    }
+
+    foreach ($nodes as $nid => $node) {
+
+      /** @var \Drupal\omnipedia_core\WrappedEntities\NodeWithWikiInfoInterface */
+      $wrappedNode = $this->typedEntityRepositoryManager->wrap($node);
+
+      $this->drupalGet($node->toUrl());
+
+      if ($wrappedNode->isWikiNode() === true) {
+
+        $this->assertSession()->responseHeaderEquals(
+          CurrentDateHeaderEventSubscriber::HEADER, $wrappedNode->getWikiDate(),
+        );
+
+      }
+
+    }
+
+  }
 
 }
