8ed8eec3fdc41b3451fad1416621e33200c1a032
Matei Stanca <i@ambientimpact.com>
2025-06-28 20:16:56 -0400

    WIP RefreshLess current date save and restore; not working:
    The assumption here is that the current date is matches the current page
    the user is looking at before it's changed, and to restore that at the
    end of the request. This assumption is probably wrong, however, because
    it doesn't seem to quite work and still results in incorrect dates.

--- modules/omnipedia_date_refreshless/src/EventSubscriber/Kernel/SetCurrentDateEventSubscriber.php
+++ modules/omnipedia_date_refreshless/src/EventSubscriber/Kernel/SetCurrentDateEventSubscriber.php
@@ -5,13 +5,18 @@ declare(strict_types=1);
 namespace Drupal\omnipedia_date_refreshless\EventSubscriber\Kernel;
 
 use Drupal\omnipedia_date\EventSubscriber\Kernel\SetCurrentDateEventSubscriber as DecoratedEventSubscriber;
+use Drupal\omnipedia_date\Service\CurrentDateInterface;
 use Drupal\refreshless\Service\RequestWrapperFactoryInterface;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\DependencyInjection\Attribute\AutowireDecorated;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\Event\TerminateEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 
 class SetCurrentDateEventSubscriber implements EventSubscriberInterface {
+
+  protected string $previousCurrentDate;
 
   /**
    * Service constructor; saves dependencies.
@@ -23,6 +28,8 @@ class SetCurrentDateEventSubscriber implements EventSubscriberInterface {
    *   The RefreshLess request wrapper factory.
    */
   public function __construct(
+    #[Autowire(service: 'omnipedia_date.current_date')]
+    protected readonly CurrentDateInterface $currentDate,
     #[AutowireDecorated]
     protected readonly DecoratedEventSubscriber $decorated,
     protected readonly RequestWrapperFactoryInterface $requestWrapperFactory,
@@ -34,21 +41,38 @@ class SetCurrentDateEventSubscriber implements EventSubscriberInterface {
   public static function getSubscribedEvents(): array {
     return [
       KernelEvents::REQUEST => 'onKernelRequest',
+      KernelEvents::TERMINATE => 'onKernelTerminate',
     ];
   }
 
   public function onKernelRequest(RequestEvent $event): void {
 
-    $requestWrapper = $this->requestWrapperFactory->fromRequest();
+    $requestWrapper = $this->requestWrapperFactory->fromRequest(
+      $event->getRequest(),
+    );
 
     if ($requestWrapper->isRefreshless() === true && (
       $requestWrapper->isPrefetch() === true ||
       $requestWrapper->isPreload() === true
     )) {
-      return;
+      $this->previousCurrentDate = $this->currentDate->get();
     }
 
     $this->decorated->onKernelRequest($event);
+
+  }
+
+  public function onKernelTerminate(TerminateEvent $event): void {
+
+    if (isset($this->previousCurrentDate)) {
+
+      $this->currentDate->set($this->previousCurrentDate);
+
+      \Drupal::logger('omnipedia_date_refreshless')->debug(
+        'Restored current date: ' . $this->previousCurrentDate,
+      );
+
+    }
 
   }
 
